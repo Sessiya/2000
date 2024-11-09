f17d417e37b55d76239c53e59eef893effef2b94
2000 <sessiya333@gmail.com>
2024-11-09 16:39:56 +0500

    Update courses.js

--- courses.js
+++ courses.js
@@ -1,8 +1,9 @@
 window.onload = function() {
     const username = localStorage.getItem('username');
     const avatar = localStorage.getItem('avatar');
+    const userActivity = JSON.parse(localStorage.getItem('userActivity'));
 
-    if (!username || !avatar) {
+    if (!username || !avatar || !userActivity) {
         // Foydalanuvchi ro'yxatdan o'tmagan bo'lsa, ro'yxatdan o'tish sahifasiga yo'naltirish
         window.location.href = 'register.html';
     } else {
@@ -11,5 +12,13 @@ window.onload = function() {
             <p>Salom, ${username}!</p>
             <img src="${avatar}" alt="Avatar" width="100" height="100">
         `;
+
+        // Show user activity
+        const activity = `
+            <p>Faoliyat:</p>
+            <p><strong>Yakunlangan mavzular:</strong> ${userActivity.completedTopics.join(', ')}</p>
+            <p><strong>Hozirgi mavzu:</strong> <span style="color: red;">${userActivity.currentTopic}</span></p>
+        `;
+        document.getElementById('userActivity').innerHTML = activity;
     }
 };
