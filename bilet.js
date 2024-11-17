8b44afc92346293cf3fe3d36672761a25272f7e2
2000 <sessiya333@gmail.com>
2024-11-17 13:00:55 +0500

    Update and rename bilet.json to bilet.js

---
+++ bilet.js
@@ -0,0 +1,7 @@
+const tickets = [
+  { question: "Savol 1", answer: "Javob 1" },
+  { question: "Savol 2", answer: "Javob 2" },
+  { question: "Savol 3", answer: "Javob 3" },
+  { question: "Savol 4", answer: "Javob 4" },
+  { question: "Savol 5", answer: "Javob 5" }
+];
