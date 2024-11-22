24d196e2a778a9a68a4c0ecf70985f72f25edc02
2000 <sessiya333@gmail.com>
2024-11-23 01:34:57 +0500

    Update dark-mode.js

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,32 +1,32 @@
-    // Tungi va kunduzgi rejimni boshqarish funksiyasi
-    function initializeDarkMode() {
-      const savedMode = localStorage.getItem("theme");
-      const body = document.body;
-      const toggleButton = document.getElementById("dark-mode-toggle");
+// Tungi va kunduzgi rejimni boshqarish funksiyasi
+function initializeDarkMode() {
+  const savedMode = localStorage.getItem("theme");
+  const body = document.body;
+  const toggleButton = document.getElementById("dark-mode-toggle");
 
-      if (savedMode === "dark") {
-        body.classList.add("dark-mode");
-        toggleButton.textContent = "☀️"; // Kunduzgi rejim tugmasi
-      } else {
-        body.classList.remove("dark-mode");
-        toggleButton.textContent = "🌙"; // Tungi rejim tugmasi
-      }
-    }
+  if (savedMode === "dark") {
+    body.classList.add("dark-mode");
+    toggleButton.textContent = "☀️"; // Kunduzgi rejim tugmasi
+  } else {
+    body.classList.remove("dark-mode");
+    toggleButton.textContent = "🌙"; // Tungi rejim tugmasi
+  }
+}
 
-    function toggleDarkMode() {
-      const body = document.body;
-      const toggleButton = document.getElementById("dark-mode-toggle");
+function toggleDarkMode() {
+  const body = document.body;
+  const toggleButton = document.getElementById("dark-mode-toggle");
 
-      body.classList.toggle("dark-mode");
+  body.classList.toggle("dark-mode");
 
-      if (body.classList.contains("dark-mode")) {
-        toggleButton.textContent = "☀️"; // Kunduzgi rejim tugmasi
-        localStorage.setItem("theme", "dark");
-      } else {
-        toggleButton.textContent = "🌙"; // Tungi rejim tugmasi
-        localStorage.setItem("theme", "light");
-      }
-    }
+  if (body.classList.contains("dark-mode")) {
+    toggleButton.textContent = "☀️"; // Kunduzgi rejim tugmasi
+    localStorage.setItem("theme", "dark");
+  } else {
+    toggleButton.textContent = "🌙"; // Tungi rejim tugmasi
+    localStorage.setItem("theme", "light");
+  }
+}
 
-    document.getElementById("dark-mode-toggle").addEventListener("click", toggleDarkMode);
-    document.addEventListener("DOMContentLoaded", initializeDarkMode);
+document.getElementById("dark-mode-toggle").addEventListener("click", toggleDarkMode);
+document.addEventListener("DOMContentLoaded", initializeDarkMode);
